485439ff1d99f9fdf6b98f27d510ed72c401d6fb
tanishkkkk <tanishk@browserstack.com>
2024-09-09 02:11:57 +0530

    feat: naviagte / to login

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
 import React from "react";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      {/* <Login /> */}
-      <Home />
+    <>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Navigate to="/login" />} />
       </Routes>
-    </div>
+    </>
   );
 }
 
