904d124a4fded9717d7266e30085206a0cfd1c72
tanishkkkk <tanishk@browserstack.com>
2024-09-09 00:40:18 +0530

    feat: action btn and logo

---
+++ src/components/ActionButton.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+interface ButtonProps {
+  text: string;
+  clickHandler: () => void;
+}
+
+const ActionButton = ({ text, clickHandler }: ButtonProps) => {
+  return (
+    <button
+      onClick={clickHandler}
+      className="bg-[#4A96FF] w-full text-white rounded font-medium p-3 my-5"
+    >
+      {text}
+    </button>
+  );
+};
+
+export default ActionButton;
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,10 +1,17 @@
-import React from "react";
 import InputField from "../components/InputField";
+import ActionButton from "../components/ActionButton";
 
 const Login = () => {
   return (
-    <main className="flex justify-center items-center h-[100vh]">
-      <div className="border- w-[463px] border-[#525458] rounded-lg flex items-center flex-col p-6 bg-[#27292D]">
+    <main className="flex justify-center flex-col items-center h-[100vh]">
+      <img
+        src="/logo.png"
+        alt="Logo"
+        style={{ width: "40px", height: "40px" }}
+        className="mb-8"
+      />
+
+      <div className="border-2 w-[463px] border-[#525458] rounded-lg flex items-center flex-col p-6 bg-[#27292D]">
         <h4 className="text-[#6B6C70] text-sm font-medium mt-4">
           WELCOME BACK
         </h4>
@@ -24,6 +31,13 @@ const Login = () => {
           type="text"
           id="password"
         />
+        <ActionButton
+          text="Login now"
+          clickHandler={() => console.log("click me")}
+        />
+        <p className="w-full text-[#6B6C70]">
+          Not registered yet? <span className="text-[#C5C7CA]">Register →</span>
+        </p>
       </div>
     </main>
   );
