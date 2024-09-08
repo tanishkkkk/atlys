63c234eea9ffc8bc964d92735e6592ce2cc9635e
tanishkkkk <tanishk@browserstack.com>
2024-09-09 02:07:13 +0530

    feat: basic route setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       {/* <Login /> */}
       <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </div>
   );
 }
